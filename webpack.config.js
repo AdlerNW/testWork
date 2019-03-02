8a5a181d700cdb8f2dcabb62bbc2496980ef6887
AdlerNW <47100536+AdlerNW@users.noreply.github.com>
2019-03-02 15:35:35 +0400

    edit config for png

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
 },
       {
-        test: /\.ttf$/,
+        test: /\.ttf|\.png$/,
         use: "file-loader"
 }
 ]
