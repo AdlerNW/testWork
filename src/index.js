ec1fedc518b1ea7d112d9344514a9fe708561054
AdlerNW <47100536+AdlerNW@users.noreply.github.com>
2019-03-03 14:28:36 +0400

    window.onload - delete DOMContentLoaded - create

--- src/index.js
+++ src/index.js
@@ -9,41 +9,42 @@ let cardNumbers = null,
     regExpThreeNamber = /\d{3}/,
     regExpForCardholderNaN = /[^a-z\s]/i,
     regExpForCardholderMin = /[a-z\s]{4,}/i;
-window.onload = function(){
+
+document.addEventListener("DOMContentLoaded", function () {
     cardNumbers = document.querySelectorAll("input.form__cardNumber");
     cardHolder = document.querySelector("input.form__cardHolder");
     CVVnumber = document.querySelector("input.form__CVVnumber");
     btnSubmit = document.querySelector("button.form__btnSubmit");
     form = document.querySelector("form.form");
     btnSubmit.addEventListener('click', checkInputs);
-}
+});
 
-function checkInputs(e){
+function checkInputs(e) {
     e.preventDefault();
     testResult = true;
     CVVnumber.className = 'form__CVVnumber';
-    for (let i = 0; i < cardNumbers.length; i++){
+    for (let i = 0; i < cardNumbers.length; i++) {
         cardNumbers[i].className = 'form__cardNumber';
     }
     cardHolder.className = 'form__cardHolder';
 
-    if (!regExpThreeNamber.test(CVVnumber.value) || (CVVnumber.value == '000')){
+    if (!regExpThreeNamber.test(CVVnumber.value) || (CVVnumber.value == '000')) {
         testResult = false;
         CVVnumber.className = 'form__CVVnumber form_error';
-    } 
+    }
 
-    for (let i = 0; i < cardNumbers.length; i++){
-        if (!regExpFourNamber.test(cardNumbers[i].value) || (cardNumbers[i].value == '0000')){
+    for (let i = 0; i < cardNumbers.length; i++) {
+        if (!regExpFourNamber.test(cardNumbers[i].value) || (cardNumbers[i].value == '0000')) {
             testResult = false;
             cardNumbers[i].className = 'form__cardNumber form_error';
         }
     }
 
-    if (regExpForCardholderNaN.test(cardHolder.value) || !regExpForCardholderMin.test(cardHolder.value)){
+    if (regExpForCardholderNaN.test(cardHolder.value) || !regExpForCardholderMin.test(cardHolder.value)) {
         testResult = false;
         cardHolder.className = 'form__cardHolder form_error';
     }
-    if (testResult){
+    if (testResult) {
         form.submit();
     }
 }
